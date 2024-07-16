6847b0cae85116cffb2a20a5c661e6398391f44f
Gabriel Rhoden <gabrielrhodden@gmail.com>
2024-07-16 20:34:02 -0300

    linux redis queue first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(20)->create();
 
-        // \App\Models\User::factory()->create([
+        // \App\Models\User::factory()->create([   php artisan db:seed
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
